63f0541d77f3d66c1e839f4975cbf0536dba9a99
unknown <prateekrai341036@gmail.com>
2022-09-11 23:50:28 +0530

    finally done

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let mealList = document.querySelector('#list');
 let errorText = document.querySelector('#error-text');
 let submit = document.querySelector('.search');
 let random = document.querySelector('.random');
-let fav = document.querySelector('#fav');
+let favList = document.querySelector('.fav-list');
 let greet = document.querySelector('#greet');
 
 
@@ -37,9 +37,14 @@ function getMealList(){
                             <img src = "${meal.strMealThumb}" alt = "food">
                         </div>
                         <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
+
+                            <h3>${meal.strMeal}</h3> 
+                            <button class="add" onclick="addList(${meal.idMeal})">
+                            ADD To Favourite Meals
+                            </button>
                             <p id="inst">${meal.strInstructions}</p>
                         </div>
+                        
                     </div>
                 
                 `;
@@ -49,7 +54,7 @@ function getMealList(){
             mealList.classList.remove('notFound');
         } else{
             errorText.innerHTML = '';
-            html = "Sorry, we didn't find any meal!";
+            html = "!!! Sorry, we didn't find any meal!";
             mealList.classList.add('notFound');
         }
 
@@ -77,7 +82,7 @@ random.addEventListener('click' , function(e){
     mealList.innerHTML = '';
     input.value ='';
     errorText.innerHTML = '';
-    for(let i =0 ; i<11 ;i++ ){
+    for(let i =0 ; i<4 ;i++ ){
         
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 		.then(res => res.json())
@@ -92,6 +97,9 @@ random.addEventListener('click' , function(e){
                                     </div>
                                     <div class = "meal-name" id="back">
                                         <h3>${meal.strMeal}</h3>
+                                        <button class="add" onclick="addList(${meal.idMeal})">
+                                        ADD To Favourite Meals
+                                        </button>
                                         <p id="inst">${meal.strInstructions}</p>
                                     </div>
                                </div>
@@ -107,6 +115,53 @@ random.addEventListener('click' , function(e){
     }
     
 });
+
+
+
+
+// now favourite list button starts here
+
+function addList(meal){
+
+    console.log(meal);
+
+    // here this meal is the id of the meal
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        let html = ''; 
+        data.meals.forEach(meal => {
+                html +=  ` <li class="list-item" id="${meal.idMeal}">
+                            <div class = "meal-item" data-id = "${meal.idMeal}">                          
+                                <div class = "meal-img">
+                                    <img src = "${meal.strMealThumb}" alt = "food">
+                                </div>
+                                <div class = "meal-name" id="back">
+                                    <h3>${meal.strMeal}</h3>
+                                    <button class="delete" onclick="deleteList(${meal.idMeal})">
+                                    Delete 
+                                    </button>
+                                    < p id="inst">${meal.strInstructions}</p>
+                                </div>
+                            </div>
+                            </li>
+                `;     
+                favList.innerHTML +=  html ;     
+        });
+    });
+
+}
+
+
+
+//Delete from favourite meals
+function deleteList(meal){                                             //here 'meal' is just a meal id passed by addlist function to here
+    let delList = document.getElementById(`${meal}`);
+    // console.log(delList);
+    favList.removeChild(delList);
+}
 
 
 
